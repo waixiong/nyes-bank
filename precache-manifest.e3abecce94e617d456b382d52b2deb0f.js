9e25a2399c99ef7b272a93c045f554b34fbc9e27
waixiong <waixiong@siswa.um.edu.my>
2019-01-30 12:20:55 +0800

    Updates

---
+++ precache-manifest.e3abecce94e617d456b382d52b2deb0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nyes-bank/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9c75ea6d5a232f9bd1b1",
+    "url": "/nyes-bank/static/js/runtime~main.9c75ea6d.js"
+  },
+  {
+    "revision": "6b2daae40bb4ac103758",
+    "url": "/nyes-bank/static/js/main.6b2daae4.chunk.js"
+  },
+  {
+    "revision": "107a2d5da08e65f28fe1",
+    "url": "/nyes-bank/static/js/1.107a2d5d.chunk.js"
+  },
+  {
+    "revision": "6b2daae40bb4ac103758",
+    "url": "/nyes-bank/static/css/main.ded51a5f.chunk.css"
+  },
+  {
+    "revision": "107a2d5da08e65f28fe1",
+    "url": "/nyes-bank/static/css/1.ad0b224e.chunk.css"
+  },
+  {
+    "revision": "1f1cca27a50b8a92b83466d6c6b49c0c",
+    "url": "/nyes-bank/index.html"
+  }
+];
